23a894853fc876f7a20fbfdec95fa564e22d60a0
aromiko <mikoaro1212@gmail.com>
2025-06-09 15:06:09 +0800

    Updates on the hero component

--- src/components/component-blocks/hero/hero.tsx
+++ src/components/component-blocks/hero/hero.tsx
@@ -1,10 +1,19 @@
+import BasicMedia from "@/components/building-blocks/basic-media/basic-media";
 import { TypeComponentHero } from "@/lib/types";
 
-export default function Hero({ heroName }: TypeComponentHero) {
+export default function Hero({
+  heroTitle,
+  heroBody,
+  heroImage
+}: TypeComponentHero) {
   return (
     <section className="justify-items-center">
-      <div className="container p-4">
-        <div>{heroName}</div>
+      <div className="container my-20 w-2xl justify-center justify-items-center space-y-2 p-4 text-center">
+        {heroImage && (
+          <BasicMedia data={heroImage} wrapperCssClass="w-100 h-100" />
+        )}
+        {heroTitle && <h2 className="text-4xl font-bold">{heroTitle}</h2>}
+        {heroTitle && <p>{heroBody}</p>}
       </div>
     </section>
   );
--- src/lib/types/component-blocks/TypeComponentHero.ts
+++ src/lib/types/component-blocks/TypeComponentHero.ts
@@ -1,7 +1,10 @@
-import { TypeComponentBase } from "@/lib/types";
+import { TypeComponentBase, TypeComponentBasicMedia } from "@/lib/types";
 
 export interface TypeComponentHero extends TypeComponentBase {
   __typename: "ComponentHero";
   sys: { id: string };
   heroName: string;
+  heroTitle?: string;
+  heroBody?: string;
+  heroImage?: TypeComponentBasicMedia;
 }
