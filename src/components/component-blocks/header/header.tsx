a470c6500f6a8e865f2a2060b7934d273ef491d1
aromiko <mikoaro1212@gmail.com>
2025-07-14 22:28:15 +0800

    Update styles

--- src/components/component-blocks/header/header.tsx
+++ src/components/component-blocks/header/header.tsx
@@ -1,5 +1,8 @@
 import BasicMedia from "@/components/building-blocks/basic-media/basic-media";
+import { buttonVariants } from "@/components/ui/button";
 import { TypeComponentHeader } from "@/lib/types";
+import { cn } from "@/lib/utils/classnames";
+import Link from "next/link";
 
 export default function Header({
   headerName,
@@ -10,7 +13,16 @@ export default function Header({
       <div className="container p-4 font-bold">
         {headerLogo && <BasicMedia data={headerLogo} />}
 
-        <div>{headerName}</div>
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "link" }),
+            "font-playfair-display text-2xl font-bold"
+          )}
+        >
+          {/* {headerName} */}
+          Q&M
+        </Link>
       </div>
     </div>
   );
