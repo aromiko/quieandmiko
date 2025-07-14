51470ba87ee827a0d56ea6f999c416c7af70184a
aromiko <mikoaro1212@gmail.com>
2025-07-14 22:32:11 +0800

    Add temporary links

--- src/components/component-blocks/header/header.tsx
+++ src/components/component-blocks/header/header.tsx
@@ -4,13 +4,10 @@ import { TypeComponentHeader } from "@/lib/types";
 import { cn } from "@/lib/utils/classnames";
 import Link from "next/link";
 
-export default function Header({
-  headerName,
-  headerLogo
-}: TypeComponentHeader) {
+export default function Header({ headerLogo }: TypeComponentHeader) {
   return (
     <div className="justify-items-center">
-      <div className="container p-4 font-bold">
+      <div className="container flex items-center justify-between p-4 font-bold">
         {headerLogo && <BasicMedia data={headerLogo} />}
 
         <Link
@@ -20,9 +17,19 @@ export default function Header({
             "font-playfair-display text-2xl font-bold"
           )}
         >
-          {/* {headerName} */}
-          Q&M
+          Quie & Miko
         </Link>
+
+        <ul>
+          <li>
+            <Link
+              href="/rsvp"
+              className={cn(buttonVariants({ variant: "link" }))}
+            >
+              RSVP
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
