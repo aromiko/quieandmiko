4785fdbe7ad95c03022047c4f6cf7efed3732c1f
aromiko <mikoaro1212@gmail.com>
2025-11-10 23:19:07 +0800

    Add metadata for save the date page

--- src/app/savethedate/page.tsx
+++ src/app/savethedate/page.tsx
@@ -1,31 +1,51 @@
-"use client";
+import SaveTheDateClient from "@/app/savethedate/savethedate-client";
+import type { Metadata } from "next";
 
-import { useIsMobile } from "@/lib/hooks/useIsMobile";
-import Image from "next/image";
-import Link from "next/link";
+export const metadata: Metadata = {
+  metadataBase: new URL("https://quieandmiko.com"),
+  title: "Save the Date | Quie & Miko",
+  description:
+    "Join us as we celebrate Quie and Miko. Save the date and view the details here.",
+  alternates: {
+    canonical: "/savethedate"
+  },
+  openGraph: {
+    type: "website",
+    url: "/savethedate",
+    siteName: "Quie & Miko Wedding",
+    title: "Save the Date | Quie & Miko",
+    description: "Save the date and view the details for Quie and Miko.",
+    images: [
+      {
+        url: "/images/std/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Quie & Miko Save the Date"
+      }
+    ],
+    locale: "en_PH"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Save the Date | Quie & Miko",
+    description: "Save the date and view the details for Quie and Miko.",
+    images: ["/images/std/og.png"],
+    creator: "@yourhandle"
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1
+    }
+  }
+};
 
 export default function SaveTheDate() {
-  const isMobile = useIsMobile();
-
-  return (
-    <div className="relative h-screen w-full">
-      <div className="absolute top-0 my-6 flex w-full items-center justify-center">
-        <Link
-          href={"/"}
-          className="transition-opacity duration-300 ease-in-out hover:opacity-50"
-        >
-          <Image
-            src="/images/monogram-wine.png"
-            alt="QM Monogram"
-            width={isMobile ? 75 : 150}
-            height={isMobile ? 75 : 150}
-          />
-        </Link>
-      </div>
-      <iframe
-        src="/std/index.html"
-        style={{ border: 0, width: "100%", height: "100%" }}
-      />
-    </div>
-  );
+  return <SaveTheDateClient />;
 }
